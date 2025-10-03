7d34d9522beff2fa14c2fbcce3d27bc92311bde1
Mohd Fahad <fahadchougle020304@gmail.com>
2025-10-04 00:50:26 +0530

    Updated the helper code

---
+++ src/helper.py
@@ -0,0 +1,54 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from typing import List
+from langchain.schema import Document
+
+
+def load_pdf_file(data):
+
+    "Load and extract text from PDF files in a directory"
+
+    loader = DirectoryLoader(data, 
+                             glob="*.pdf", 
+                             loader_cls=PyPDFLoader)
+    documents = loader.load()
+    return documents
+
+
+def filter_minimal_docs(docs: List[Document]) -> List[Document]:
+
+    "Given a list of Documents, return a list of Documents with only page_content and source metadata."
+
+    minimal_doc : List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get("source")
+        minimal_doc.append(
+            Document(
+                page_content = doc.page_content, 
+                metadata={"source": src}
+                )
+            ) 
+    return minimal_doc
+
+
+def text_split(minimal_docs):
+
+    "Split documents into smaller chunks for better processing"
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 500,
+        chunk_overlap = 20
+    )
+    text_chunks = text_splitter.split_documents(minimal_docs)
+    return text_chunks
+
+
+def download_embeddings():
+
+    "Download embeddings model from HuggingFace embeddings model"
+
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+
+    embeddings = HuggingFaceEmbeddings(model_name=model_name)
+    return embeddings
---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+system_prompt = (
+    "You are a helpful and knowledgeable **medical assistant chatbot**. "
+    "Use the retrieved context to answer medical questions clearly and accurately. "
+    "If the answer is not in the context, say you don't know. "
+    "Keep responses short (max 3 sentences), clear, and easy to understand. "
+    "Do not provide personal medical advice, diagnosis, or treatment. "
+    "Always encourage users to consult a qualified healthcare professional for medical concerns.\n\n"
+    "{context}"
+)
