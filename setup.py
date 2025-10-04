f9c86c1034ed73b00b9733b1b374909e5e96445b
Mohd Fahad <fahadchougle020304@gmail.com>
2025-10-04 13:54:14 +0530

    Updated setup file

--- setup.py
+++ setup.py
@@ -7,5 +7,16 @@ setup(
     author="Mohd Fahad Chougle",
     author_email="fahadchougle020304@gmail.com",
     packages=find_packages(),
-    install_requires=[]
+    install_requires=[
+        "langchain-groq",
+        "langchain==0.3.26",
+        "sentence-transformers==4.1.0",
+        "pypdf==5.6.1",
+        "python-dotenv==1.1.0",
+        "langchain-community==0.3.26",
+        "fastapi==0.115.0",
+        "uvicorn==0.32.0",
+        "chromadb==0.5.17",
+        "faiss-cpu"
+    ]
 )
